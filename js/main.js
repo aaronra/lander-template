fc9bfef18d5ed11652b4b10c951bcd805e4d69e2
Jr Reyes <jr@internetsoftwaresystems.com>
2015-07-28 17:15:08 +0800

    template lander

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function clickME() {
+    alert('\
+            **************************************\n\n\
+                Congratulations\n\n\
+               \n\n\
+            **************************************');
+    window.location.replace('win.html');
+}
